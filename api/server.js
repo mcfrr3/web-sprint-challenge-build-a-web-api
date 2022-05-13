296175a0358d2ea4c15125d83b02a678ac6c5b08
Mike Ferrete <m.ferrete88@gmail.com>
2022-05-12 19:30:44 -0800

    project init and setting env vars

--- api/server.js
+++ api/server.js
@@ -3,7 +3,12 @@ const server = express();
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
+const actionsRouter = require('../api/actions/actions-router');
 // Build your projects router in /api/projects/projects-router.js
+const projectsRouter = require('../api/projects/projects-router');
 // Do NOT `server.listen()` inside this file!
+
+server.use('/api/actions', actionsRouter);
+server.use('/api/projects', projectsRouter);
 
 module.exports = server;
